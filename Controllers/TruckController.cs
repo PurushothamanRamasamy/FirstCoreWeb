a39bc58df99934d8aed2981ea44521ed35c110f6
Purushothaman <purushothaman.ramasamy@kanini.com>
2021-03-22 18:47:54 +0530

    Add project files.

---
+++ Controllers/TruckController.cs
@@ -0,0 +1,102 @@
+﻿using FirstCoreWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreWeb.Controllers
+{
+    public class TruckController : Controller
+    {
+        public Truck truck = new Truck();
+        public static List<Truck> trucks = Truck.AllTrucks();
+
+        // GET: TruckController
+        public IActionResult Trucks()
+        {
+            
+            return View(trucks);
+        }
+
+        // GET: TruckController/Details/5
+        public ActionResult Details(string id)
+        {
+           Truck detailsTruck= trucks.FirstOrDefault(trk => trk.TruckNumber == id);
+            return View(detailsTruck);
+        }
+
+        // GET: TruckController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TruckController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Truck addtruck)
+        {
+            try
+            {
+
+                truck.AddTruck(addtruck);
+                return RedirectToAction("Trucks");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TruckController/Edit/5
+        public ActionResult Edit(string id)
+        {
+            Truck detailsTruck = trucks.FirstOrDefault(trk => trk.TruckNumber == id);
+            return View(detailsTruck);
+        }
+
+        // POST: TruckController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Truck edittruck)
+        {
+            try
+            {
+                
+                truck.UpdateTruck(edittruck);
+                return RedirectToAction("Trucks");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TruckController/Delete/5
+        public ActionResult Delete(string id)
+        {
+            Truck detailsTruck = trucks.FirstOrDefault(trk => trk.TruckNumber == id);
+            truck.DeleteTruck(detailsTruck);
+            return RedirectToAction("Trucks");
+            /*return View(detailsTruck);*/
+        }
+
+        // POST: TruckController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Truck removetruck)
+        {
+            try
+            {
+                truck.DeleteTruck(removetruck);
+                return RedirectToAction("Trucks");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Truck.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreWeb.Models
+{
+    public class Truck
+    {
+        public string TruckNumber { get; set; }
+        public string TruckName { get; set; }
+        public bool Status { get; set; }
+        public string DriverName { get; set; }
+
+        public Truck(string tno, string tname, string dname, bool sts)
+        {
+            TruckNumber = tno;
+            TruckName = tname;
+            DriverName = dname;
+            Status = sts;
+        }
+
+        
+
+        public Truck()
+        {
+         }
+        public static List<Truck> trucks = new List<Truck>();
+       
+        public static List<Truck> AllTrucks()
+        {
+            //trucks.Clear();
+            trucks.Add(new Truck("TN29AY7802", "KKR Transports", "Raju", true));
+            trucks.Add(new Truck("TN09CY5412", "Vinayaga Transports", "Maaran", false));
+            trucks.Add(new Truck("TN30PR0007", "PRO Transports", "Raju", true));
+            trucks.Add(new Truck("PY15GG1254", "SNR Transports", "Kumar", false));
+
+
+            return trucks;
+        }
+
+        public void AddTruck(Truck truck)
+        {
+            trucks.Add(truck);
+        }
+        public void DeleteTruck(Truck truck)
+        {
+            trucks.Remove(truck);
+        }
+        public void UpdateTruck(Truck truck)
+        {
+            foreach (var item in trucks)
+            {
+                if (item.TruckNumber==truck.TruckNumber)
+                {
+                    item.TruckName = truck.TruckName;
+                    item.Status = truck.Status;
+                    item.DriverName = truck.DriverName;
+                }
+            }
+            /*trucks.Where(trk => trk.TruckNumber == truck.TruckNumber)
+                  .Select(t => { 
+                      t.Status = truck.Status;
+                      t.TruckName = truck.TruckName;
+                      t.DriverName = truck.DriverName; 
+                      
+                      return t; 
+                  });*/
+        }
+    }
+}
